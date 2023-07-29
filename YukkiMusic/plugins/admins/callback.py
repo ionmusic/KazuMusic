22518fe5393c3d9c3a04a646efa63aa1d4a64569
IONBOT <113283570+ionmusic@users.noreply.github.com>
2023-07-30 05:58:12 +0700

    Update callback.py

--- YukkiMusic/plugins/admins/callback.py
+++ YukkiMusic/plugins/admins/callback.py
@@ -6,22 +6,22 @@ from pyrogram.types import CallbackQuery, InlineKeyboardMarkup
 from config import (AUTO_DOWNLOADS_CLEAR, BANNED_USERS,
                     SOUNCLOUD_IMG_URL, STREAM_IMG_URL,
                     TELEGRAM_AUDIO_URL, TELEGRAM_VIDEO_URL, adminlist)
-from PrimeMusic import YouTube, app
-from PrimeMusic.core.call import Prime
-from PrimeMusic.misc import SUDOERS, db
-from PrimeMusic.utils.database import (is_active_chat,
+from YukkiMusic import YouTube, app
+from YukkiMusic.core.call import Prime
+from YukkiMusic.misc import SUDOERS, db
+from YukkiMusic.utils.database import (is_active_chat,
                                        is_music_playing, is_muted,
                                        is_nonadmin_chat, music_off,
                                        music_on, mute_off, mute_on,
                                        set_loop)
-from PrimeMusic.utils.decorators.language import languageCB
-from PrimeMusic.utils.formatters import seconds_to_min
-from PrimeMusic.utils.inline.play import (panel_markup_1,
+from YukkiMusic.utils.decorators.language import languageCB
+from YukkiMusic.utils.formatters import seconds_to_min
+from YukkiMusic.utils.inline.play import (panel_markup_1,
                                           panel_markup_2,
                                           stream_markup,
                                           telegram_markup)
-from PrimeMusic.utils.stream.autoclear import auto_clean
-from PrimeMusic.utils.thumbnails import gen_thumb
+from YukkiMusic.utils.stream.autoclear import auto_clean
+from YukkiMusic.utils.thumbnails import gen_thumb
 
 wrong = {}
 
