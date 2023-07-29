929814b9c23e75cf6d9e366ed3120984e1d691ed
IONBOT <113283570+ionmusic@users.noreply.github.com>
2023-07-30 06:01:58 +0700

    Update active.py

--- YukkiMusic/plugins/tools/active.py
+++ YukkiMusic/plugins/tools/active.py
@@ -2,9 +2,9 @@ from pyrogram import filters
 from pyrogram.types import Message
 
 from strings import get_command
-from PrimeMusic import app
-from PrimeMusic.misc import SUDOERS
-from PrimeMusic.utils.database.db_memory import (
+from YukkiMusic import app
+from YukkiMusic.misc import SUDOERS
+from YukkiMusic.utils.database.db_memory import (
     get_active_chats, get_active_video_chats)
 
 # Commands
